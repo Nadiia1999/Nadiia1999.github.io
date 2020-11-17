13b0bd7c7344547eeedd89a46f3842b282f8d27a
Nadiia1999 <72097069+Nadiia1999@users.noreply.github.com>
2020-11-17 21:33:01 +0200

    Add files via upload

---
+++ laboratorna_3.js
@@ -0,0 +1,32 @@
+(function(){
+'use strict';	
+angular.module('myFirstApp',[])
+.controller('myFirstController', myFirstController); 
+
+ myFirstController.$inject=['$scope' ];
+
+function myFirstController($scope){
+ $scope.words="";
+$scope.Productlist="";
+
+ $scope.breakfast= function(){
+ $scope.words=probil($scope.Productlist);
+ };
+ 
+ function probil(products1){
+	 let products=products1.split(',');
+	 
+	 for(let i=products.length - 1; i>=0; i--){
+		 if (products[i]==="") products.splice(i, 1);
+	 }
+	 if (products.length===0){
+		 return "Будь ласка, уведіть дані!";
+	 } else if (products.length<=3){
+		 return "На здоров'я!";
+	 } else {
+		 return "Це забагато!";
+     }
+	 
+ }
+}
+}) ();
